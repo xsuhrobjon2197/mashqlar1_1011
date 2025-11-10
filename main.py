45e38f47034c8112a931e54ab99cc36839d78108
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2025-11-10 04:45:55 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+#1-misol
+i = input("misol kirit: ")
+print(len(i))
+
+#2-misol
+i = "uzr dunyo"
+print(i[0],i[-1])
+
+#3-misol
+i = input("matn kirit: ")
+print(i.upper())
+
+#4-misol
+i = "SALOM DUNYO"
+print(i.lower())
+
+#5-misol
+i = "  salom dunyo"
+print(i.strip())
+
+#6-misol
+i = "salom dunyo"
+print(len(i))
